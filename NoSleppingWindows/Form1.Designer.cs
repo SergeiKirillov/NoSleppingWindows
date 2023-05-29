2fc93a60b696500dcf98c64459c58baffb3e6bc3
sergei.kirillov.a <sergei.kirillov.a@gmail.com>
2023-05-29 14:22:21 +0600

    begin

--- NoSleppingWindows/Form1.Designer.cs
+++ NoSleppingWindows/Form1.Designer.cs
@@ -77,6 +77,7 @@
             this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
             this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(462, 426);
             this.axWindowsMediaPlayer1.TabIndex = 1;
+            this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
             // 
             // frmMain
             // 
--- NoSleppingWindows/Form1.cs
+++ NoSleppingWindows/Form1.cs
@@ -18,5 +18,10 @@ namespace NoSleppingWindows
 
             axWindowsMediaPlayer1.URL= "NoSleepWindows.mp4";
         }
+
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+
+        }
     }
 }
