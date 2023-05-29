e50b36b243bb9028498a301d87675ef8ccbef1dd
sergei.kirillov.a <sergei.kirillov.a@gmail.com>
2023-05-29 14:22:06 +0600

    begin

--- NoSleppingWindows/Form1.Designer.cs
+++ NoSleppingWindows/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NoSleppingWindows
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.выборФайлаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.конфигурацияяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выборФайлаToolStripMenuItem,
+            this.конфигурацияяToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(462, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // выборФайлаToolStripMenuItem
+            // 
+            this.выборФайлаToolStripMenuItem.Name = "выборФайлаToolStripMenuItem";
+            this.выборФайлаToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.выборФайлаToolStripMenuItem.Text = "Выбор Файла";
+            // 
+            // конфигурацияяToolStripMenuItem
+            // 
+            this.конфигурацияяToolStripMenuItem.Name = "конфигурацияяToolStripMenuItem";
+            this.конфигурацияяToolStripMenuItem.Size = new System.Drawing.Size(106, 20);
+            this.конфигурацияяToolStripMenuItem.Text = "Конфигурацияя";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 24);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(462, 426);
+            this.axWindowsMediaPlayer1.TabIndex = 1;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(462, 450);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "НеСпать";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem выборФайлаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem конфигурацияяToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
     }
 }
 
--- NoSleppingWindows/Form1.cs
+++ NoSleppingWindows/Form1.cs
@@ -10,11 +10,13 @@ using System.Windows.Forms;
 
 namespace NoSleppingWindows
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+
+            axWindowsMediaPlayer1.URL= "NoSleepWindows.mp4";
         }
     }
 }
